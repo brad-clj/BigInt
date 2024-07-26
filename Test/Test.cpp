790f7bcd3da8533b1d48bc6774f6a02b041c3eb7
Brad <brad.clj@gmail.com>
2024-07-25 19:14:17 -0700

    AddAssignWorks

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <utility>
 #include <gtest/gtest.h>
 #include "BigInt.h"
 
@@ -35,7 +36,29 @@ TEST(BigIntStringCtor, ThrowsExceptionOnInvalidArgument)
 {
     EXPECT_THROW(BigInt(""), std::invalid_argument);
     EXPECT_THROW(BigInt("-"), std::invalid_argument);
-    EXPECT_THROW(BigInt("abcd"), std::invalid_argument);
+    EXPECT_THROW(BigInt("foo"), std::invalid_argument);
     EXPECT_THROW(BigInt("0x42"), std::invalid_argument);
     EXPECT_THROW(BigInt("123456789012345678901234567890x"), std::invalid_argument);
+}
+
+TEST(BigIntAddOps, AddAssignWorks)
+{
+    // if using the same object for the two arguments the rhs gets copy constructed as a temporary
+    BigInt acc1("75755724578284142547987951683356371041");
+    acc1 += acc1;
+    EXPECT_TRUE(acc1 == BigInt("151511449156568285095975903366712742082"));
+    // carry past lim is pop_back'ed with negative rhs
+    acc1 += BigInt("-151511449156568285095975903366712742082");
+    EXPECT_TRUE(acc1 == BigInt(0));
+    // acc2 has this replaced with acc1 as it is an rvalue and has higher capacity
+    BigInt acc2(42);
+    acc2 += std::move(acc1);
+    EXPECT_TRUE(acc2 == BigInt(42));
+    // flip to negative
+    acc2 += BigInt(-43);
+    EXPECT_TRUE(acc2 == BigInt(-1));
+    // if this doesn't go positive internally add an additional -1 past the back and set negative bit
+    BigInt acc3(-4293984256);
+    acc3 += BigInt(-4279238656);
+    EXPECT_TRUE(acc3 == BigInt(-8573222912));
 }
